4e28c9909e3fddf0e86391594028bfaf9d2b2d04
Jim Hessin <jhessin@gmail.com>
2024-04-20 08:43:39 -0600

    Sat 20 Apr 2024 08:43:41 AM MDT

--- starfield/current_project.py
+++ starfield/current_project.py
@@ -2,20 +2,29 @@ from resource_manager import ResourceManager
 
 rm = ResourceManager()
 
-# Weapon Workbench
+# Semi-automatic receiver
+# rm.r['copper'] += 3
+# rm.r['titanium'] += 4
+# rm.r['nickel'] += 3
+# rm.r['sealant'] += 2
+
+# Recon Laser Sight
 rm.r['adhesive'] += 3
-rm.r['iron'] += 4
-rm.r['sealant'] += 3
-rm.r['nickel'] += 2
+rm.r['paladium'] += 2
+rm.r['vanadium'] += 3
+rm.r['zero_wire'] += 4
+
 
 # Already added to research
 # rm.r['isotopic_coolant'] -= 4
 
 # Checklist - Give it all to Vasco
 rm.r['adhesive'] -= 0
-rm.r['iron'] -= 4
-rm.r['nickel'] -= 2
-rm.r['sealant'] -= 3
+# rm.r['copper'] -= 4
+# rm.r['paladium'] -= 2
+rm.r['silver'] -= 0
+# rm.r['vanadium'] -= 3
+rm.r['zero_wire'] -= 0
 
 # Breakdown manufactured goods
 
@@ -27,6 +36,11 @@ rm.r['sealant'] -= 3
 #     research.r['supercooled_magnet'] += 1
 #     research.r['tau_grade_rheostat'] += 1
 #     research.r['microsecond_regulator'] -= 1
+# 
+for x in range(rm.r['zero_wire']):
+    rm.r['copper'] += 1
+    rm.r['silver'] += 1
+    rm.r['zero_wire'] -= 1
 
 # Tier 2
 
